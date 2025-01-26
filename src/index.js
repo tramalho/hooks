180462da0682fa1b127eb76bfe765b46e4bd396f
tramalho <thiagofacul@gmail.com>
2025-01-26 15:33:30 -0300

    Configuracao do Projeto

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import "./index.css";
+import App from "./views/App";
+import { createRoot } from "react-dom/client";
+ 
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(<App />);
